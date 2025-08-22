be534dd61ddbf62e0de263a441d714b6642a067d
Ali Al-Iusefi <alighanem99@live.com>
2025-08-22 16:59:38 +0900

    fix: fix url validation

--- myurlshortner-be/src/main/java/org/acme/domain/service/ShortenedUrlValidator.java
+++ myurlshortner-be/src/main/java/org/acme/domain/service/ShortenedUrlValidator.java
@@ -7,8 +7,9 @@ import java.util.List;
 
 public class ShortenedUrlValidator {
     private final static Integer MAX_URL_SIZE = 1000;
-    private final static String HTTP_PREFIX = "http://";
-    private final static String HTTPS_PREFIX = "https://";
+    private final static String HTTP_PREFIX = "http";
+    private final static String HTTPS_PREFIX = "https";
+    private final static String SEPARATOR_PREFIX = "://";
 
     public static List<ShortenedUrlException> validateUrl(String url) {
         if (url.isBlank()) {
@@ -18,20 +19,23 @@ public class ShortenedUrlValidator {
         if (url.length() > MAX_URL_SIZE) {
             listOfErrors.add(new ShortenedUrlException.UrlIsTooLongException(MAX_URL_SIZE, url.length()));
         }
-        if (!url.startsWith(HTTP_PREFIX) && !url.startsWith(HTTPS_PREFIX)) {
-            listOfErrors.add(new ShortenedUrlException.UrlIsNotHttpException(url));
+
+        String removedHttp = url;
+        if (url.contains(SEPARATOR_PREFIX)) {
+            String[] split = url.split(SEPARATOR_PREFIX);
+            String protocol = split[0];
+            if (!protocol.equals(HTTP_PREFIX)) {
+                listOfErrors.add(new ShortenedUrlException.UrlIsNotHttpException(url));
+            }
+            if (!protocol.equals(HTTPS_PREFIX)) {
+                listOfErrors.add(new ShortenedUrlException.UrlIsNotHttpException(url));
+            }
+            removedHttp = split[1];
+        }
+
+        if (removedHttp.isBlank()) {
+            listOfErrors.add(new ShortenedUrlException.UrlIsMissingHostNameException(url));
             return listOfErrors;
-        } else {
-            String removedHttp;
-            if (url.startsWith(HTTPS_PREFIX)) {
-                removedHttp = url.substring(HTTPS_PREFIX.length());
-            } else {
-                removedHttp = url.substring(HTTP_PREFIX.length());
-            }
-            if (removedHttp.isBlank()) {
-                listOfErrors.add(new ShortenedUrlException.UrlIsMissingHostNameException(url));
-                return listOfErrors;
-            }
         }
 
         return listOfErrors;
