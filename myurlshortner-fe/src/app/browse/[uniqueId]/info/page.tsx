3b988c58d6ba8cffdf8068fc7b04021dc042b032
Ali <35502268+alialiusefi@users.noreply.github.com>
2025-10-19 15:25:52 +0900

    feature(myurlshortner-fe): shortened url history fe in info page (#58)
    * feature: implement info page
    
    * chore: upd spec
    
    * chore: handle 404 error and move to parent
    
    * chore: delete no needed page
    
    * chore: update spec to redirect to /browse instead

---
+++ myurlshortner-fe/src/app/browse/[uniqueId]/info/page.tsx
@@ -0,0 +1,18 @@
+import ShortenedUrlInfo from "components/ShortenedUrlInfoComponent/ShortenedUrlInfo";
+import Instant from "ts-time/Instant";
+import ZonedDateTime from "ts-time/ZonedDateTime";
+import { LOCAL_ZONE_ID } from "ts-time/Zone";
+
+export default async function Info({
+  params,
+}: {
+  params: Promise<{ uniqueId: string }>;
+}) {
+  const { uniqueId } = await params;
+  const instant = Instant.now();
+  const now = ZonedDateTime.ofInstant(
+    instant,
+    LOCAL_ZONE_ID.offsetAtInstant(instant),
+  ).toString();
+  return <ShortenedUrlInfo uniqueId={uniqueId} now={now} />;
+}
--- myurlshortner-fe/src/app/urls/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Page() {
-  return <h1>Work in progress</h1>;
-}
