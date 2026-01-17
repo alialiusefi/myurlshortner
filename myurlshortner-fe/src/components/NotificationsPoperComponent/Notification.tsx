dff071d628e6d9a8d0210a2ff5ae6afd9c8d53b2
Ali Al-Iusefi <alighanem99@live.com>
2026-01-17 10:24:57 +0300

    feature(fe): target user notification and modal for info and action

--- myurlshortner-fe/src/components/NotificationsPoperComponent/Notification.tsx
+++ myurlshortner-fe/src/components/NotificationsPoperComponent/Notification.tsx
@@ -1,12 +1,18 @@
 export enum NotificationType {
-  SHORTENED_URL_REACHED_N_VIEWS,
+  SHORTENED_URL_REACHED_N_VIEWS = "SHORTENED_URL_REACHED_N_VIEWS",
+  GIFT_REQUEST_TO_TARGET_USER = "GIFT_REQUEST_TO_TARGET_USER",
+}
+
+export interface ShortenedUrlNotification {
+  id: number;
+  type: NotificationType;
+  read_at: string | null;
 }
 
 export type ShortenedUrlReachedNViewsParams = {
   unique_identifier: string;
   views: number;
 };
-
 export class ShortenedUrlReachedNViewsNotification
   implements ShortenedUrlNotification
 {
@@ -16,8 +22,16 @@ export class ShortenedUrlReachedNViewsNotification
   read_at: string | null;
 }
 
-export interface ShortenedUrlNotification {
+export type GiftRequestToTargetUserParams = {
+  unique_identifier: string;
+  gift_request_id: number;
+  source_user_id: number;
+};
+export class GiftRequestToTargetUserNotification
+  implements ShortenedUrlNotification
+{
   id: number;
   type: NotificationType;
+  params: GiftRequestToTargetUserParams;
   read_at: string | null;
 }
--- myurlshortner-fe/src/components/ShortenedUrlInfoCardComponent/ShortenedUrlInfoCard.tsx
+++ myurlshortner-fe/src/components/ShortenedUrlInfoCardComponent/ShortenedUrlInfoCard.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Grid, Paper, Typography, Card } from "@mui/material";
+import { Grid, Typography, Card } from "@mui/material";
 import { redirect } from "next/navigation";
 import {
   GetShortenedUrlInfo404Response,
@@ -25,51 +25,49 @@ export default function ShortenedUrlInfoCard(params: { uniqueId: string }) {
     redirect("/browse");
   }
   return (
-    <Paper>
-      <Card>
-        <Typography variant="h3" sx={{ p: 2 }}>
-          Info
+    <Card>
+      <Typography variant="h3" sx={{ p: 2 }}>
+        Info
+      </Typography>
+      <Grid container spacing={1}>
+        <Typography variant="h5" paddingLeft={2}>
+          Unique Identifier:
         </Typography>
-        <Grid container spacing={1}>
-          <Typography variant="h5" paddingLeft={2}>
-            Unique Identifier:
-          </Typography>
-          <Typography variant="h5" paddingLeft={2} fontFamily="monospaced">
-            {params.uniqueId}
-          </Typography>
-        </Grid>
-        <Grid container spacing={1} padding={2}>
+        <Typography variant="h5" paddingLeft={2} fontFamily="monospaced">
+          {params.uniqueId}
+        </Typography>
+      </Grid>
+      <Grid container spacing={1} padding={2}>
+        <Typography variant="h5">
+          <NewTabLink url={data?.shortened_url} />
+        </Typography>
+        {data?.is_enabled ? (
+          <ArrowForwardIcon fontSize="large" color={"success"} />
+        ) : (
+          <DoNotDisturbOnIcon fontSize="large" color={"error"} />
+        )}
+        <Grid size={5}>
           <Typography variant="h5">
-            <NewTabLink url={data?.shortened_url} />
-          </Typography>
-          {data?.is_enabled ? (
-            <ArrowForwardIcon fontSize="large" color={"success"} />
-          ) : (
-            <DoNotDisturbOnIcon fontSize="large" color={"error"} />
-          )}
-          <Grid size={5}>
-            <Typography variant="h5">
-              <NewTabLink url={data?.url} />
-            </Typography>
-          </Grid>
-        </Grid>
-        <Grid container>
-          <Typography
-            gutterBottom
-            paddingLeft={2}
-            sx={{ color: "text.secondary", fontSize: 14 }}
-          >
-            Created At: {readableTimestamp(data?.created_at)}
-          </Typography>
-          <Typography
-            gutterBottom
-            paddingLeft={2}
-            sx={{ color: "text.secondary", fontSize: 14 }}
-          >
-            Last Updated At: {readableTimestamp(data?.updated_at)}
+            <NewTabLink url={data?.url} />
           </Typography>
         </Grid>
-      </Card>
-    </Paper>
+      </Grid>
+      <Grid container>
+        <Typography
+          gutterBottom
+          paddingLeft={2}
+          sx={{ color: "text.secondary", fontSize: 14 }}
+        >
+          Created At: {readableTimestamp(data?.created_at)}
+        </Typography>
+        <Typography
+          gutterBottom
+          paddingLeft={2}
+          sx={{ color: "text.secondary", fontSize: 14 }}
+        >
+          Last Updated At: {readableTimestamp(data?.updated_at)}
+        </Typography>
+      </Grid>
+    </Card>
   );
 }
