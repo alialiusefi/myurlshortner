f43bd41d44cda92e35d9a2342dcbcaa1a10de7f1
Ali Al-Iusefi <alighanem99@live.com>
2026-02-17 15:12:16 +0300

    chore(be): fix migration

--- postgres/db/migration/V14__create_shortened_urls_indexed_table.sql
+++ postgres/db/migration/V14__create_shortened_urls_indexed_table.sql
@@ -4,9 +4,6 @@ create table shortened_urls_indexed (
 );
 
 create index shortened_urls_indexed_title_index on shortened_urls_indexed using gin (title);
-);
-
-create index shortened_urls_indexed_title_index on shortened_urls_indexed using gin (title);
 
 merge into shortened_urls_indexed using shortened_urls
 	on shortened_urls_indexed.unique_identifier = shortened_urls.unique_identifier
